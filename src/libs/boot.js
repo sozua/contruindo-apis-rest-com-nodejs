cb93fd8d1ea4b62da5d534b42ec572ce58817416
Diogo de Souza <diogo@sozua.dev>
2021-01-16 22:44:28 -0300

    refactor: Refatorado o projeto para suportar os modelos Users e Tasks criados

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  app.db.sync().done(() => {
+  app.db.sequelize.sync().done(() => {
     app.listen(app.get("port"), () => {
       console.log(`Servidor online na porta ${app.get("port")}`);
     });
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -2,10 +2,8 @@ module.exports = (app) => {
   const Tasks = app.models.tasks;
 
   app.get("/tasks", (req, res) => {
-    Tasks.findAll({}, (tasks) =>
-      res.json({
-        tasks,
-      })
-    );
+    Tasks.findAll({}).then((tasks) => {
+      res.json({ tasks: tasks });
+    });
   });
 };
