3d10d69cefa189516aa0360a1d524ace56a7a3c5
Diogo de Souza <diogo@sozua.dev>
2021-01-17 22:16:23 -0300

    6.1 - Rotas de tarefas organizadas

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,28 @@
 module.exports = (app) => {
-  const Tasks = app.models.tasks;
-
-  app.get("/tasks", (req, res) => {
-    Tasks.findAll({}).then((tasks) => {
-      res.json({ tasks: tasks });
+  const Tasks = app.db.models.Tasks;
+  app
+    .route("/tasks")
+    .all((req, res) => {
+      // Middleware de pré-execução das rotas
+    })
+    .get((req, res) => {
+      // "/tasks": Lista tarefas
+    })
+    .post((req, res) => {
+      // "/tasks": Cadastra uma nova tarefa
     });
-  });
+  app
+    .route("/tasks/:id")
+    .all((req, res) => {
+      // Middleware de pré-execução das rotas
+    })
+    .get((req, res) => {
+      // "/tasks/1": Consulta uma tarefa
+    })
+    .put((req, res) => {
+      // "/tasks/1": Atualiza uma tarefa
+    })
+    .delete((req, res) => {
+      // "/tasks/1": Exclui uma tarefa
+    });
 };
