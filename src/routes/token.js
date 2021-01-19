6523ed78d6b491b460b2e7181aaee2866f40254b
Diogo de Souza <diogo@sozua.dev>
2021-01-19 17:49:57 -0300

    10.2 - Gerada a documentação dos tokens

--- src/routes/token.js
+++ src/routes/token.js
@@ -3,6 +3,24 @@ import jwt from "jwt-simple";
 module.exports = (app) => {
   const cfg = app.libs.config;
   const Users = app.db.models.Users;
+
+  /**
+   * @api {post} /token Token autenticado
+   * @apiGroup Credencial
+   * @apiParam {String} email Email de usuário
+   * @apiParam {String} password Senha de usuário
+   * @apiParamExample {json} Entrada
+   * {
+   * "email": "john@connor.net",
+   * "password": "123456"
+   * }
+   * @apiSuccess {String} token Token de usuário autenticado
+   * @apiSuccessExample {json} Sucesso
+   * HTTP/1.1 200 OK
+   * {"token": "xyz.abc.123.hgf"}
+   * @apiErrorExample {json} Erro de autenticação
+   * HTTP/1.1 401 Unauthorized
+   */
   app.post("/token", (req, res) => {
     if (req.body.email && req.body.password) {
       const email = req.body.email;
