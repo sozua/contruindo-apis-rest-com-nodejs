e4faaf43192899a3cf4ae2e6fea22338b64d79c8
Diogo de Souza <diogo@sozua.dev>
2021-01-19 18:24:01 -0300

    Modificado a mensagem de boot

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   if (process.env.NODE_ENV !== "test") {
     app.db.sequelize.sync().done(() => {
       app.listen(app.get("port"), () => {
-        console.log(`NTask API - porta ${app.get("port")}`);
+        console.log(`Servidor online na porta ${app.get("port")}`);
       });
     });
   }
